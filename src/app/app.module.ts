e86329a6a71780739b555409bbfbcb9fec5aa9f5
olfara <olfara@users.noreply.github.com>
2018-12-25 21:37:43 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './Modules/layout/layout.component';
+import { UserComponent } from './Modules/user/user.component';
+import { HomeComponent } from './Modules/home/home.component';
+import {RoutesModule} from './routes/routes.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    UserComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule, RoutesModule, FormsModule, ReactiveFormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
