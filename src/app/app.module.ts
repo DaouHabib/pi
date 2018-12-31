bc19bb9c6e5080a793d43929f282490c6b402a25
habib daou <36645987+DaouHabib@users.noreply.github.com>
2018-12-31 14:28:22 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,12 @@ import { HomeComponent } from './Modules/home/home.component';
 import {RoutesModule} from './routes/routes.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { MapMandateComponent } from './Modules/Mandates/map-mandate/map-mandate.component';
+import { AllMandatesComponent } from './Modules/Mandates/all-mandates/all-mandates.component';
+import { RequestComponent } from './Modules/request/request.component';
+import {DndModule} from 'ng2-dnd';
+import { MapClientComponent } from './Modules/map-client/map-client.component';
+import { AddRequestComponent } from './Modules/add-request/add-request.component';
 
 
 
@@ -18,10 +24,15 @@ import { HttpClientModule } from '@angular/common/http';
     AppComponent,
     LayoutComponent,
     UserComponent,
-    HomeComponent
+    HomeComponent,
+    MapMandateComponent,
+    AllMandatesComponent,
+    RequestComponent,
+    MapClientComponent,
+    AddRequestComponent
   ],
   imports: [
-    BrowserModule, HttpClientModule, RoutesModule, FormsModule, ReactiveFormsModule
+    BrowserModule, HttpClientModule, RoutesModule, FormsModule, ReactiveFormsModule,DndModule.forRoot()
 
   ],
   providers: [],
